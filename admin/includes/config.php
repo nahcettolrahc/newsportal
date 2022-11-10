373c1519ba20852c2c14f289c79290af3061dd1a
pavilonletoan <pavilion@charlotte.com>
2022-11-10 23:29:19 +0700

    sql

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_USER','ducthinh');
+define('DB_PASS' ,'ducthinh12320@E');
 define('DB_NAME','newsportal');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // kiem tra ket noi
